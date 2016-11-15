af5257ebadbb3a6922e3ed00a88850c357584486
mreilaender <manuel.reilaender@gmail.com>
2016-11-15 10:38:41 +0100

    first commit

---
+++ src/main/java/com/accenture/mreilaender/Main.java
@@ -0,0 +1,37 @@
+package com.accenture.mreilaender;
+
+/**
+ * @author manuel
+ * @version 11/15/16
+ */
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Hello World!");
+        Button btn = new Button();
+        btn.setText("Say 'Hello World'");
+        btn.setOnAction(new EventHandler<ActionEvent>() {
+
+            public void handle(ActionEvent event) {
+                System.out.println("Hello World!");
+            }
+        });
+
+        StackPane root = new StackPane();
+        root.getChildren().add(btn);
+        stage.setScene(new Scene(root, 300, 250));
+        stage.show();
+    }
+}
